2c077f11a37fdac8ed5e4f26d090e6d3d8192f8c
jajoyce <jjoyce4@gmail.com>
2022-02-11 12:46:03 -0500

    Create templates. Set up URLs and template views for home and about. Set up static files.

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
