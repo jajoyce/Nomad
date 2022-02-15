7c949a3e9265b2c196613fb7c19612014f14e3c6
vvillbii <will.bowles90@gmail.com>
2022-02-15 15:12:57 -0500

    post seed data added and truncated card body text

--- db/post.sql
+++ db/post.sql
@@ -1,2 +1,114 @@
-INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('San Francisco Travel Guide
-', 'There''s no denying that, at least in the 21st century, San Francisco is often thought of as the technology hub of America. And it''s perfectly true that this city—which is home to Apple, Facebook, Google, Uber, and countless other technology platforms that impact our daily lives—is synonymous with big tech. However, San Francisco is home to revelatory architecture, the first established LGBTQ+ neighborhood in the country, and Michelin-starred dining, all of which are crucial components of San Francisco''s legacy.', '2021-05-07 18:57:42.517205-07', 2, 1, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wysetc.org%2Fwp-content%2Fuploads%2Fsites%2F19%2F2018%2F07%2FAdobeStock_152013093.jpeg&f=1&nofb=1');
+-- INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('San Francisco Travel Guide
+-- ', 'There''s no denying that, at least in the 21st century, San Francisco is often thought of as the technology hub of America. And it''s perfectly true that this city—which is home to Apple, Facebook, Google, Uber, and countless other technology platforms that impact our daily lives—is synonymous with big tech. However, San Francisco is home to revelatory architecture, the first established LGBTQ+ neighborhood in the country, and Michelin-starred dining, all of which are crucial components of San Francisco''s legacy.', '2021-05-07 18:57:42.517205-07', 2, 1, 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.wysetc.org%2Fwp-content%2Fuploads%2Fsites%2F19%2F2018%2F07%2FAdobeStock_152013093.jpeg&f=1&nofb=1');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES ('New York City Travel Guide', 'New York is the most visited city in the United States.
+
+Famous for its fashion, crazy nightlife, incredible art scene, museums, incredible restaurant scene, and world-class theater productions, to me, New York City is the beating heart of the world, which is why I called it home for six years. Every culture, language, and food is represented here. You could spend a lifetime exploring the city and never really see it all.
+
+There is a never-ending list of things to do in the city. No amount of time you spend here will be enough so don''t worry about running out of things to do. Whatever you like, you can find it in NYC.
+
+But, as a budget traveler, visiting NYC can be tough, especially when you don''t know the hidden gems that make living here affordable. There are plenty of things to do that won''t cost you an arm and a leg — if you know where to look!
+
+This travel guide to NYC can help you make the most of your visit without breaking the bank!', '2022-02-07 13:57:42.517205-07', 3, 2, 'https://image.cnbcfm.com/api/v1/image/106268734-1574876711571gettyimages-1059614218.jpeg?v=1576856860');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('15 Things To See & Do In NYC On A Budget', 'New York is one of my favorite cities. In fact, I love it so much I lived there for close to five years. I can never get enough of it!
+
+To me, NYC is the center of the world, with people, foods, and cultures from everywhere. It has a wild nightlife scene, and there''s a never-ending list of things to see, do, and experience.
+
+You can never be bored in NYC. 
+
+But, as awesome as it is, it can easily break the most hardened of budgets without proper planning.
+
+Tourists often decry New York''s cost, but this is also place of starving artists and underpaid interns. To be a local is to know where the deals are. 
+
+Despite what most people think, it''s actually possible to have a fun, budget-friendly trip to the Big Apple without breaking the bank — if you know where to look.', '2022-02-09 10:00:42.517205-07', 5, 2, 'https://expertvagabond.com/wp-content/uploads/new-york-city-guide-1536x1025.jpg.webp');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Tips for Traveling Paris on a Budget
+', 'The City of Lights is a destination that everyone needs to experience at least once! With an incredible food scene to its high fashion and fascinating history, there''s so much to see and explore.
+
+But, like most capital cities around the world, Paris is not the cheapest destination. Meals are the top restaurants will make a massive dent in your wallet, and all the entrance fees for the attractions and museums don''t help either.
+
+That doesn''t mean you should write off Paris from your bucket list. It''s possible to visit the city on a shoestring if you''re prepared to tweak your itinerary here and there.', '2021-02-09 10:00:42.517205-07', 2, 4, 'https://media.cntraveler.com/photos/5cf96a9dd9fb41f17ed08435/master/pass/Eiffel%20Tower_GettyImages-1005348968.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('The 10 Best, Cheap, and Fun Hostels in Tokyo', 'Tokyo is one of the most expensive cities in the world.
+
+But there are still amazing and affordable hostels in Tokyo where you can stay and lower your costs!
+
+Like Tokyo itself, the hostels here are clean, arty, and have lots of charm. A lot of them are super trendy with beautiful décor. Wi-Fi is standard, and many will have cooking facilities too. Beds generally cost between 2,000 and 5,000 JPY ($18-45 USD) per night. They host events, serve a ton of tea, and have little cubbies you can sleep in.
+
+Whether you''re looking for a relaxing stay or to party the night away, the city has something for everyone!
+
+Tokyo does hostels right.', '2021-02-09 10:00:42.517205-07', 5, 8, 'https://media.nomadicmatt.com/2019/tokyohostel1b.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('30 Unique Things to See and Do During Your Visit to Tokyo', 'Tokyo is an incredible, massive city home to over 10 million people. It''s the capital of Japan and home to a wide array of fun, historic, and quirky attractions. From the world''s largest tuna auction and the Imperial Palace to robot restaurants and vampire cafés, Tokyo really does have something for everyone.
+
+Whether you''re here for a short stopover or living here as an expat, you''ll be able to find plenty of ways to pass the time and get a feel for the organized chaos that is Tokyo. To help you make the most out of your trip, here are the best things to see and do in Tokyo.', '2022-01-09 11:00:42.517205-07', 6, 8, 'https://media.nomadicmatt.com/2019/tokyothingstodo1.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('The Best Walking Tours in Paris', 'I love visiting Paris. I''ve been going there for over nine years and have yet to tire of the history, the charm, or the food (and wine!).
+
+Over the years, I''ve taken a million different tours of this amazing city to learn more about what makes it tick. For four months in 2019, I lived in Paris and had the opportunity to check out even more walking tours.
+
+And there was a lot to check out!
+
+There are dozens upon dozens of companies covering all aspects of life in Paris, and it can be hard to make a sense of all those endless Viator and TripAdvisor listings. It''s crazy how many tour companies there are!
+
+I mean, I''ve tried so many tour companies and I STILL have more on my list to try (and some to retry).
+
+That said, having sampled a ton of tours (and having organized my own tours in Paris for a while), I wanted to share the best walking tour companies in Paris to help you plan your trip and save money in this iconic destination.', '2022-01-01 12:00:42.517205-07', 4, 4, 'https://media.nomadicmatt.com/2019/paristour2.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Is Bangkok safe to visit?', 'Bangkok can be an overwhelming destination for tourists, and it is depicted in many Western films as seedy, gritty, and intense, with wild nightlife and never-ending traffic.
+
+Most visitors don''t stay there for long, but if you''re patient and begin to peel back the layers, you''ll discover a complex, beautiful city filled with some of the friendliest and most thoughtful people you will ever meet (Thailand is called the Land of Smiles for a reason!) and delicious street food.
+
+But is Bangkok a safe place to visit?
+
+Just like any major city, you must be alert and aware of your surroundings, but it is pretty safe, and the risk of serious danger is very low (unless you are up to something illegal). You are most likely to encounter small scams designed to get a few extra bucks from you (more on that later).', '2022-01-01 15:15:42.517205-07', 6, 3, 'https://media.nomadicmatt.com/2020/bangkoksafe.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('How to Save Money in Dubai', 'Dubai is a big stopover destination for travelers flying Emirates Airlines as they transit around the world (or Etihad passengers if they decide to visit from Abu Dhabi). It was how I ended up there. I was transiting to/from the Maldives.
+
+As I explored the city, I came to realize the rumors I heard were true: Dubai was expensive.
+
+But like every destination (except maybe Bermuda), I found plenty of ways to save and visit on a budget.
+
+In Dubai, the ways to save are not obvious. They are hidden far beneath the surface as the city. Much like Las Vegas, Dubai is designed to extract as much money from you as possible.
+
+Fortunately, I had some local friends share their secret tips with me during my trip. Today, I''m going to share those tips to help you save money in Dubai — and I''ll share some of my favorite activities too!', '2022-01-01 13:45:42.517205-07', 6, 6, 'https://media.nomadicmatt.com/2020/dubaimoneysave1.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('10 Offbeat Things to Do in Istanbul', 'Istanbul is home to some famous historical sites — the Blue Mosque, Hagia Sophia, Grand Bazaar, and Spice Market. They are stunning, important historical sites to see and experience. But the city also offers lots of fun things to do that have fewer crowds and are a bit off the beaten path.
+
+As important as it is to visit culturally significant historical sites (after all, there''s nothing wrong with being a tourist) there is so much more to every destination than just the main tourist bullet points.
+
+Of course, you shouldn''t miss Istanbul''s major sites. But once you''ve done them, there are plenty of offbeat things to see and do here. Here are my favorites to help you get started and make the most out of your trip.', '2022-01-15 15:15:42.517205-07', 5, 7, 'https://media.nomadicmatt.com/2019/offbeatistanbul2.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Staying at Raffles Dubai in the UAE', 'Dubai never ceases to inspire me. With man-made islands, epic buildings, and world-renowned restaurants, this city has it all. It''s like a city designed by a kid with an incredible imagination. Being here taps into the childlike wonder in me. It takes a certain magic to build the best and the biggest.
+
+I always jump at a chance to visit Dubai and, on my latest visit, I laid my head to rest and relax at Raffles Dubai.
+
+A quick five-minute trip from the airport, this is an especially handy spot for anyone traveling on business. It''s also just about 10 to 15 minutes from the new development at Creek Harbour and a quick trip into Downtown Dubai.
+
+Best of all, from Raffles Dubai, the view of the city skyline will absolutely take your breath away every time you look out the window. It is almost otherworldly in its beauty.', '2021-12-20 17:22:42.517205-07', 2, 6, 'https://www.visitdubai.com/-/media/images/leisure/detail-pages/explore-dubai/1-1-about-dubai-t14/1-1-about-dubai-t14-fallback.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('30 Things to See and Do in San Francisco', 'I love San Francisco. It''s a city home to hippies, techies, artists, immigrants, students, and everyone in between. There''s incredible music, delicious food for all budgets, some of the BEST Asian food in the country, parks galore, and an amazing bohemian vibe.
+
+With so many diverse influences, it''s no surprise that San Francisco has evolved into a world-class city that has a ton of amazing things to see and do.
+
+Personally, I love visiting San Francisco for the food. It''s home to some of the best Asian and Mexican food in the country (as well as some incredible cafes).
+
+But there is also much more to see here too. It''s a wonderful city for a weekend getaway.
+
+But just because it''s an expensive place to live doesn''t mean a visit has to break the bank.
+
+To help you plan your trip, here are the best 30 things to see and do in San Francisco', '2021-12-05 13:48:42.517205-07', 3, 1, 'https://a.cdn-hotels.com/gdcs/production70/d1574/6e020215-bcc7-4b63-8869-386f5f3afb6e.jpg?impolicy=fcrop&w=800&h=533&q=medium');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Porto Travel Guide', 'Sitting at the mouth of the Duoro River, Porto is Portugal''s second-largest and second-most-popular city. It''s the home of Port wine, which became popular after the 17th century, and you can find plenty of places to sample this popular dessert wine.
+
+While Porto lacks the iconic “wow!” factor of Lisbon, I still think it''s a beautiful riverside city filled with lots of good fo
+od, plentiful wine, and cozy riverfront cafes.
+
+Moreover, this is the best place to start your trips to the nearby Douro Valley where you can explore and tour the region''s famous port vineyards. It''s also one of the main launching grounds for the Camino Portugues (a multi-day hike to Santiago de Compostela in Spain), making it a popular hub for hikers and pilgrims alike.
+
+This travel guide to Porto will help you plan your trip, save money, and ensure you make the most out of your visit here!', '2021-11-15 08:48:42.517205-07', 6, 10, 'https://media.nomadicmatt.com/portoguide.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Why is Antalya so Popular?', 'If you are one of many asking why is Antalya so popular, we can list hundreds of reasons. But let us start with some stats that prove its popularity. So far, in 2019, 14 million people visited the region. That is the same amount that visits Istanbul, Turkey''s top tourist destination. Such is the demand, airlines are looking at new flight schedules for next year, as they prepare for another record-breaking year.
+
+The rising popularity also features among foreign home buyers. Antalya, Turkey''s second busiest housing market attracts many nationalities. They are interested in holiday homes, permanent residences, buy-to-let rental properties and making a wise real estate investment. Many foreigners looking to tap into Turkey''s citizenship by investment scheme also choose Antalya and the region''s thriving ex-pat community of thousands live here all year round.', '2021-11-03 09:18:42.517205-07', 5, 9, 'https://img.spotblue.com/app/uploads/2019/12/Kaleici-old-town.jpg.webp');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Things to Do in Antalya', 'Antalya is a sea-side heaven located on the coast of Mediterrenean Sea in southern Turkey. It is the country''s second biggest tourist draw after Istanbul, and welcomes millions of visitors each year, known as the gateway to the Turkish Riviera.
+
+When it comes to the Mediterranean, Antalya''s Turquoise Coast offers one of the most sensational coastlines in the region, but beyond its beaches and jade water is a classically beautiful city, rich with history (think ancient Roman ruins), bustling with markets, and rich in traditional Turkish food.
+
+Many visit Antalya and set up at their sea-side resort, though for those wanting to balance out their time at the beach, the following are great things to do.', '2021-06-07 11:33:42.517205-07', 3, 9, 'https://i1.wp.com/www.mappingmegan.com/wp-content/uploads/2020/09/Antalya-Turkey-Hot-air-balloon-RF.jpg?resize=1024%2C717&ssl=1');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('Where to Find the Best Afternoon Tea in London', 'What would a trip to London be without afternoon tea? London''s restaurants, hotels, and cafes are full of indulgent and decant experiences.
+
+From traditional afternoon tea at Kensington Palace to unleashing your inner wizard, the possibilities are endless! It''s a once in a lifetime foodie experience, and it''s worth every penny.', '2021-05-27 09:29:22.517205-07', 2, 5, 'https://media.architecturaldigest.com/photos/56fd9d65ecd154e0329c1627/4:3/w_768/london-travel-guide-lede.jpg');
+INSERT INTO main_app_post (title, content, created_at, author_id, city_id, img) VALUES('How to Avoid the Crowds in London', 'One of the world''s busiest cosmopolitan cities (pandemic aside), traveling to London means there''s always hustle and bustle to navigate and deal with.
+
+Between dodging through cars queued in traffic, squashing your way onto an over-crowded train, and trying to score tickets on the West End, London is an amazing and diverse city, but it''s constantly busy!
+
+London attracts around 30 million visitors from around the world every year, and has a population of 8.9 million locals. If you''re planning a trip to this great city, here are some tips for not getting lost in the crowd, and avoiding the busy!', '2021-03-22 19:13:38.517205-07', 6, 5, 'http://babyloncitytours.com/wp-content/uploads/2016/09/london-441853_960_720.jpg');
