ef5c7cc479ae837515166e3327b74f0b49c1b0e7
jajoyce <jjoyce4@gmail.com>
2022-02-17 12:07:01 -0500

    Update home page view and template to show real posts. Update profile delete confirmation, fix text. Update styling.

--- main_app/views.py
+++ main_app/views.py
@@ -14,6 +14,11 @@ from  django import forms
 class Home(TemplateView):
     template_name = "home.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(id__lte=3)
+        return context
+
 class About(TemplateView):
     template_name = "about.html"
 
